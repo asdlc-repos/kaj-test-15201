c8c2b51e5ff8eeb47861e82f3a7cf29d1c52d9cb
kaje94 <a.kajendran@gmail.com>
2026-04-24 10:12:20 +0530

    update entrypoint

--- todo-web/entrypoint.sh
+++ todo-web/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Strip trailing slash from TODO_API_URL before substituting into nginx template
+TODO_API_URL="${TODO_API_URL%/}"
+
 # Substitute $TODO_API_URL into nginx config template
 envsubst '$TODO_API_URL' \
   < /etc/nginx/conf.d/default.conf.template \
